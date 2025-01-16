e2c71e39c517a6947411907de3d0510a7ea494d8
osadeh <omer_sadeh@intuit.com>
2025-01-16 19:41:19 +0200

    remove commnets

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,21 +38,15 @@ def render_screen():
     if "messages" not in st.session_state:
         reset_session()
 
-    # Display the existing chat messages via `st.chat_message`.
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
     if prompt := st.chat_input("מה תרצי לענות?"):
-
-        # Store and display the current prompt.
         st.session_state.messages.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
             st.markdown(prompt)
 
-        # Generate a response using the OpenAI API.
         stream = client.chat.completions.create(
             model="gpt-4o-mini",
             messages=[{"role": "system", "content": get_system_prompt("omer-base")}] +
@@ -63,8 +57,6 @@ def render_screen():
             stream=True,
         )
 
-        # Stream the response to the chat using `st.write_stream`, then store it in
-        # session state.
         with st.chat_message("assistant"):
             response = st.write_stream(stream)
         st.session_state.messages.append({"role": "assistant", "content": response})
