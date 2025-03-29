162ead8645a7d3ac178f530911c1fd06a1d47df4
Omer-Sadeh <omer.sadeh@protonmail.com>
2025-03-29 21:04:35 +0300

    tips on no cmpletion

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,9 @@ client = OpenAI(api_key=st.secrets["openai_key"])
 db = firestore.client()
 prompts = db.collection("prompts")
 
-BASE_PROMPT = "text_VR_prompt_with_feedback"
+BASE_PROMPT = "omer-base-eng"
+
+INPUT_BLOCKED = False
 
 hebrew_prompt = "האמת אתמול בערב ממש התבאסתי על דנה, קבענו להיפגש לקפה אתמול אחרי מלא זמן שלא נפגשנו. ממש קשה לי גם ככה לקבוע תוכניות ולצאת מהבית בתקופה הזו. חצי שעה לפני הזמן שקבענו, אחרי שכבר התארגנתי ובאתי לצאת היא כתבה לי שהיא ממש מצטערת אבל היא לא יכולה, בעלה היה בעבודה או משהו והיא הייתה צריכה לשמור על הילדים. לא משנה, זאת כבר הפעם השלישית שהיא עושה לי את זה. אני כבר לא יודעת מה לחשוב.. "
 english_prompt = "Yesterday evening I was really upset with Dana, we had arranged to meet for coffee yesterday after a long time we hadn't met. It's really hard for me to make plans and leave the house during this period. Half an hour before the time we arranged, after I had already arranged and come to leave, she wrote to me that she was really sorry but she couldn't, her husband was at work or something and she had to take care of the kids. Never mind, this is already the third time she's done this to me. I don't know what to think anymore.. "
@@ -32,32 +34,45 @@ def reset_session():
     st.session_state.messages = [
         {"role": "assistant", "content": hebrew_prompt if is_hebrew() else english_prompt}
     ]
+    st.session_state.current_stage = 0
     st.session_state.guidelines = [
-        "Identifying the conflict and initial frustration - Did the therapist recognize and validate Noa’s frustration regarding the conflict?",
-        "Identifying and framing the emotions involved in the conflict - Did the therapist help Noa articulate and frame her emotions instead of rushing to solutions?",
-        "Recognizing Noa’s coping or behavioral patterns in conflicts - Did the therapist explore Noa’s typical ways of handling conflicts?",
-        "Defining a goal for the conflict - Did the therapist help Noa define a goal for the conversation?",
-        "Practicing assertive communication - Did the therapist prompt Noa to practice formulating an assertive message for the conversation?",
-        "Creating a compromise or summary that supports a healthy resolution - Did the therapist assist Noa in drawing conclusions and considering a compromise?"
+        [
+            "Identifying the conflict and initial frustration - Did the therapist recognize and validate Noa’s frustration regarding the conflict?"
+        ],
+        [
+            "Identifying and framing the emotions involved in the conflict - Did the therapist help Noa articulate and frame her emotions instead of rushing to solutions?",
+            "Recognizing Noa’s coping or behavioral patterns in conflicts - Did the therapist explore Noa’s typical ways of handling conflicts?",
+            "Defining a goal for the conflict - Did the therapist help Noa define a goal for the conversation?"
+        ],
+        [
+            "Practicing assertive communication - Did the therapist prompt Noa to practice formulating an assertive message for the conversation?",
+            "Creating a compromise or summary that supports a healthy resolution - Did the therapist assist Noa in drawing conclusions and considering a compromise?"
+        ]
     ]
+    st.session_state.rounds_since_last_completion = 0
 
 def evaluate_guidelines():
     system = f"You are an expert in human emotions and psychology. \
     I will show you a part of a therapy session with Noa, a patient who is struggling with a conflict. \
     Your goal is to evaluate the therapist's performance according to the following guidelines:"
 
-    if len(st.session_state.guidelines ) == 0:
-        return []
+    current_guidelines = st.session_state.guidelines[st.session_state.current_stage]
 
-    for idx, guideline in enumerate(st.session_state.guidelines ):
+    for idx, guideline in enumerate(current_guidelines):
         system += f"\n\n{idx+1}. {guideline}"
+
+    system += f"\n\nMark a guideline as completed if and only if the therapist has completed it, and it has absolutely \
+    been a part of the conversation, not indirectly but very obviously. \
+    A guideline is completed only if has been attempted by the therapist. \
+    If the guideline was completed by Noa with no help from the therapist, \
+    please do not mark it as completed."
 
     therapy_session = ""
     for message in st.session_state.messages:
         therapy_session += f"{'Noa' if message['role'] == 'assistant' else 'Therapist'}: {message['content']}\n\n"
 
     answer = client.chat.completions.create(
-        model="gpt-4o-mini",
+        model="o3-mini",
         messages=[{"role": "system", "content": system}, {"role": "user", "content": therapy_session}],
         response_format={
             "type": "json_schema",
@@ -86,16 +101,66 @@ def evaluate_guidelines():
     updated_guidelines = []
     completed_guidelines = []
 
-    for idx, guideline in enumerate(st.session_state.guidelines):
+    for idx, guideline in enumerate(current_guidelines):
         if idx + 1 not in completed_idxs:
             updated_guidelines.append(guideline)
         else:
             completed_guidelines.append(guideline.split(" - ")[0])
 
-    st.session_state.guidelines = updated_guidelines
+    st.session_state.guidelines[st.session_state.current_stage] = updated_guidelines
 
-    return completed_guidelines
+    completed = False
 
+    if len(updated_guidelines) == 0:
+        st.session_state.current_stage += 1
+        if st.session_state.current_stage >= len(st.session_state.guidelines):
+            completed = True
+
+    return completed_guidelines, completed
+
+def get_director_tip():
+    system = f"You are an expert in human emotions and psychology. \
+    I will show you a part of a therapy session with Noa, a patient who is struggling with a conflict. \
+    Your goal is to give the therapist the best tip for improving the therapy session. \
+    The therapist has the following guidelines to accomplish:"
+
+    current_guidelines = st.session_state.guidelines[st.session_state.current_stage]
+
+    for idx, guideline in enumerate(current_guidelines):
+        system += f"\n\n{idx+1}. {guideline}"
+
+    system += f"\n\nIf the therapist has completely lost direction and is not behaving as a therapist should, \
+    please give a tip to the therapist on how to get back on track. \
+    Be gentle but direct, and keep it a one short sentence."
+
+    therapy_session = ""
+    for message in st.session_state.messages:
+        therapy_session += f"{'Noa' if message['role'] == 'assistant' else 'Therapist'}: {message['content']}\n\n"
+
+    answer = client.chat.completions.create(
+        model="o3-mini",
+        messages=[{"role": "system", "content": system}, {"role": "user", "content": therapy_session}],
+        response_format={
+            "type": "json_schema",
+            "json_schema": {
+                "name": "tip_for_therapist",
+                "schema": {
+                    "type": "object",
+                    "properties": {
+                        "tip": {
+                            "type": "string",
+                            "description": "The tip for the therapist to improve the therapy session."
+                        }
+                    },
+                    "required": ["tip"],
+                    "additionalProperties": False
+                },
+                "strict": True
+            }
+        }
+    ).choices[0].message.content
+
+    return json.loads(answer)["tip"]
 
 def update_directions():
     if is_hebrew():
@@ -208,6 +273,8 @@ def sidebar():
                 st.sidebar.warning("Please confirm to delete the prompt.")
 
 def render_screen():
+    global INPUT_BLOCKED
+
     update_directions()
 
     sidebar()
@@ -238,29 +305,44 @@ def render_screen():
             st.markdown(message["content"])
         conversation_text += f"{message['role']}: {message['content']}\n"
 
-    if prompt := st.chat_input("מה תרצי לענות?"):
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
+    if not INPUT_BLOCKED:
+        if prompt := st.chat_input("מה תרצי לענות?"):
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            with st.chat_message("user"):
+                st.markdown(prompt)
 
-        stream = client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages=[{"role": "system", "content": st.session_state.system_prompt}] + st.session_state.messages,
-            stream=True,
-        )
+            stream = client.chat.completions.create(
+                model="gpt-4o-mini",
+                messages=[{"role": "system", "content": st.session_state.system_prompt}] + st.session_state.messages,
+                stream=True,
+            )
 
-        with st.chat_message("assistant"):
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
-        conversation_text += f"user: {prompt}\nassistant: {response}\n"
+            with st.chat_message("assistant"):
+                response = st.write_stream(stream)
+            st.session_state.messages.append({"role": "assistant", "content": response})
+            conversation_text += f"user: {prompt}\nassistant: {response}\n"
 
-        completed = evaluate_guidelines()
+            INPUT_BLOCKED = True
 
-        for guideline in completed:
-            st.success(f"Guideline '{guideline}' completed successfully.")
+            completed_guidelines, done = evaluate_guidelines()
 
-        if len(st.session_state.guidelines) == 0:
-            st.success("All guidelines completed successfully!")
+            if len(completed_guidelines) > 0:
+                st.session_state.rounds_since_last_completion = 0
+            else:
+                st.session_state.rounds_since_last_completion += 1
+
+            for guideline in completed_guidelines:
+                st.success(f"Guideline '{guideline}' completed successfully.")
+
+            if done:
+                st.success("All guidelines completed successfully!")
+
+            if st.session_state.rounds_since_last_completion > 0:
+                tip = get_director_tip()
+                st.warning(f"Tip: {tip}")
+                INPUT_BLOCKED = False
+            else:
+                INPUT_BLOCKED = False
 
 if __name__ == "__main__":
     render_screen()
